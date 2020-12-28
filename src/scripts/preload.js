6c7b6c7aa19b1f098f295f8f5750f54e47dab7d3
Alex Devero <deveroalex@gmail.com>
2020-12-28 15:06:48 +0100

    Add renderer and preload scripts

---
+++ src/scripts/preload.js
@@ -0,0 +1,7 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+// const epubjs = require('epubjs')
+
+window.addEventListener('DOMContentLoaded', () => {
+  // window.epubjs = epubjs
+})
