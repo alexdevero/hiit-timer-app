f0796c556d4711930a778548041b58e35716a219
Alex Devero <deveroalex@gmail.com>
2020-12-28 15:23:10 +0100

    Add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => (
+  <footer className="pt-4 pb-4">
+    <div className="container">
+      <p className="small text-center">Copyright &copy;{new Date().getFullYear()} Devero Studio. All rights reserved.</p>
+    </div>
+  </footer>
+)
